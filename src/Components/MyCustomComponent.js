c9fa8ba551ac834cccaebd5c5578871a8331b903
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2022-04-01 10:22:28 -0300

    Initial Commit

---
+++ src/Components/MyCustomComponent.js
@@ -0,0 +1,5 @@
+function MyCustomComponent() {
+  return <h1>A</h1>;
+}
+
+export default MyCustomComponent;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as MyCustomComponent } from "./Components/MyCustomComponent.js";
